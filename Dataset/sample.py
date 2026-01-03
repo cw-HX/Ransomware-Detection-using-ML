342a23ea30f7deff76fe5f48ebadbde3d0b21588
Anupriya Gupta <240231001@hbtu.ac.in>
2026-01-03 19:50:59 +0530

    .

--- Dataset/sample.py
+++ Dataset/sample.py
@@ -1,3 +1,11 @@
+import os
 import pandas as pd
-read = pd.read_csv("C:/Users/Manish/OneDrive/Desktop/Vit/Tanmay-SET-RaaS/Dataset")
-read.head(5)
+
+def main():
+    here = os.path.dirname(__file__)
+    path = os.path.join(here, "MalwareData.csv")
+    df = pd.read_csv(path)
+    print(df.head(5))
+
+if __name__ == '__main__':
+    main()
